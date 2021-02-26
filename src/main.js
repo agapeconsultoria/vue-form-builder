2dd535a4e334220ba499c2c066a0cffab5116524
lucaspanetto@ucl.br <lucaspanetto@ucl.br>
2021-02-26 14:46:59 -0300

    fix: descomentado i18n para compilar localmente

--- src/main.js
+++ src/main.js
@@ -14,15 +14,15 @@ Vue.config.productionTip = false
 Vue.use(VueI18n);
 Vue.use(VueFormBuilderPlugin)
 
-/* const messages = {
+ const messages = {
   ptBR: {
     canvas: {
       add_section: 'Adicionar Seção',
       add_control: 'Adicionar Controle',
-      normal_block_name: 'Bloco Normal',
-      normal_block_description: 'Bloco Normal com Título',
-      toggleable_block_name: 'Bloco Alternável',
-      toggleable_block_description: 'Bloco de seção com recurso alternar (exibir / ocultar)',
+      normal_block_name: 'FIXA',
+      normal_block_description: 'Bloco padrão com título e subtítulo',
+      toggleable_block_name: 'Expansível',
+      toggleable_block_description: 'Bloco sanfonado com título',
       droppable_zone: 'As zonas / controles para soltar serão mostrados aqui ...'
     },
     buttons:{
@@ -54,9 +54,9 @@ Vue.use(VueFormBuilderPlugin)
 const i18n = new VueI18n({
   locale: 'ptBR',
   messages
-}); */
+}); 
 
 new Vue({
-  render: h => h(App)
-/*   i18n
- */}).$mount('#app')
+  render: h => h(App),
+  i18n
+}).$mount('#app')
